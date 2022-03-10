e29be8012aaaeaa02925b043e01ece257fcae7fb
AUSTINEODUOR <austineoduor57@gmail.com>
2022-03-10 14:34:05 -0800

    my 1st c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+/**
+*main - entry point
+*
+*
+*Return -always 0 (success)
+*/
+
+void main(void)
+{
+	puts("Programming is like building a multiligual puzzle");
+
+	return (0);
+}
