1875ec85aae6eeef5684853d6221b6aa8382ad88
AUSTINEODUOR <austineoduor57@gmail.com>
2022-03-11 14:07:49 -0800

    e.q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: - Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	if (ch == 'e' || ch == 'q')
+	{
+	}
+	else
+	{
+	putchar(ch);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
