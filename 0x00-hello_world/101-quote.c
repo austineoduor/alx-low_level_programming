237763bd643ba521c57bc48bf0618076c0637b48
AUSTINEODUOR <austineoduor57@gmail.com>
2022-03-11 01:34:05 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
-#include<unistd.h>
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - Entry point
+*main - Entry point
 *
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	system("rm barhamounafissa > sorti 2>&1");
-	exit(1);
+	return (1);
 }
