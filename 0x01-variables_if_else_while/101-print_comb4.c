64532206644d1f3aed820d808274bfcac3d2629b
AUSTINEODUOR <austineoduor57@gmail.com>
2022-03-12 12:52:21 -0800

    comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,18 +8,18 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 48 ; a < 58 ; a++)
+	for (a = '0' ; b <= '9' ; a++)
 	{
-		for (b = 49 ; b < 58 ; b++)
+		for (b = '0' ; b <= '9' ; b++)
 		{
-			for (c = 50 ; c < 58 ; c++)
+			for (c = '0' ; c <= '9' ; c++)
 			{
-			if (a > b && b > c)
+			if (a < b && b < c)
 			{
 				putchar(a);
 				putchar(b);
 				putchar(c);
-				if (a != 55 || b != 56)
+				if (a != '7')
 				{
 
 					putchar(',');
