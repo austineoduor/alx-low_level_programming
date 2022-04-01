0581adcaa14019867d0668c7727582ecfea74343
AUSTINEODUOR <austineoduor57@gmail.com>
2022-04-01 05:24:13 -0700

    seconda

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main -entry point
+*@argc: count number of arguments
+*@argv: value of arguments
+*Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	
+	{
+		if (argc == 1)
+		{
+		printf("%d\n", (argc = 0));
+		}
+		else
+		printf("%d\n", argc -= 1);
+	}
+	return (0);
+}
