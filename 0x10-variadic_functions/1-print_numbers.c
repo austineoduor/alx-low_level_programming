3b414cc4c67122823eff92098fdc139449838811
AUSTINEODUOR <austineoduor57@gmail.com>
2022-04-14 07:12:51 -0700

    vast

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+*print_numbers - summing function
+*@n :number
+*Return: alwys 0 (Success)
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int num = 0, i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+		if (separator !=NULL)
+		{
+			printf("%s",separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
