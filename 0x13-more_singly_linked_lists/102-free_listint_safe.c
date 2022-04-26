7d673f5c74d82bd1957ef0b74ba35b8a1ec87984
AUSTINEODUOR <austineoduor57@gmail.com>
2022-04-26 04:12:11 -0700

    first

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,71 +1,40 @@
 #include "lists.h"
 
-
 /**
- * printData - print
- * @pointer: listint_t pointer
-*/
-void printData(const listint_t *pointer)
-{
-	printf("[%p] %d\n", (void *)pointer, pointer->n);
-}
-
-/**
- * totoise_and_hare_detect_cycle - detect cycle in linked list
- * @head: linked list
- * Return: size_t size of elements
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
  */
-size_t totoise_and_hare_detect_cycle(const listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-	size_t size, i;
-	const listint_t *slow, *fast;
-	listint_t *tmp;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-	slow = head;
-	fast = head;
-	size = 0;
+	if (!h || !*h)
+		return (0);
 
-	while (slow && fast && fast->next)
+	while (*h)
 	{
-		printData(slow);
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			slow = head;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-			}
-			tmp = (listint_t *)slow;
-			for (i = 0; i < size - 1; i++)
-				tmp = tmp->next;
-			printData(tmp);
-			printData(tmp->next);
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			exit(98);
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
 		}
-		size++;
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
 	}
-	tmp = (listint_t *)slow;
-	for (i = 0; i < size; i++)
-	{
-		printData(tmp);
-		tmp = tmp->next;
-	}
-	return (size += 2);
-}
 
-/**
- * print_listint_safe - Print all items in linked list
- * @head: const listint_t*  linked list
- * Return: size_t size of list
- */
-size_t print_listint_safe(const listint_t *head)
-{
-	if (!head)
-		exit(98);
-	return (totoise_and_hare_detect_cycle(head));
+	*h = NULL;
+
+	return (len);
 }
