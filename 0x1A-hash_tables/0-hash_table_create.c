7f3fb0ac375693d5bb1b3309006ac641be2a5a1c
AUSTINEODUOR <austineoduor57@gmail.com>
2022-07-01 07:06:22 -0700

    file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,31 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+hash_table_t *table_struct = NULL;
+	hash_node_t **new_table = NULL;
 	unsigned long int i;
-	hash_table_t *hashtable;
 
-	if (size == 0)
-		return (NULL);
-	hashtable = malloc(sizeof(hash_table_t));
-	if (hashtable == NULL)
-		return (NULL);
-	hashtable->size = size;
-	hashtable->array = malloc(sizeof(hash_node_t *) * size);
-	if (hashtable->array == NULL)
-		return (NULL);
 
+	/* allocating memory for the struct that holds the new table */
+	table_struct = malloc(sizeof(hash_table_t));
+
+	if (!table_struct)
+		return (NULL);
+	table_struct->size = size;
+	/*allocating memory for the double pointer that serves as the table*/
+	new_table = malloc(sizeof(hash_node_t *) * size);
+
+	if (!new_table)
+	{
+		free(table_struct);
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
-		hashtable->array[i] = NULL;
+		new_table[i] = NULL;
 	}
-	return (hashtable);
+
+	table_struct->array = new_table;
+
+	return (table_struct);
 }
