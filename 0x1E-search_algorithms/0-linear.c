c5086db98e93ebc723f6b34564caa74489fff10c
austineoduor <austineoduor57@gmail.com>
2022-10-05 15:59:25 +0300

    file

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,7 +20,9 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
+		{
 			return (i);
+		}
 		i++;
 	}
 	return (-1);
