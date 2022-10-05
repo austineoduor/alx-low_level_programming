cba39d28d45e5c23628ee16de1b6fea0c0ca1654
austineoduor <austineoduor57@gmail.com>
2022-10-05 14:54:57 +0300

    files

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -28,5 +28,6 @@ int exponential_search(int *array, size_t size, int value);
 /*nt interpolation_search(int *array, size_t size, int value);*/
 int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
