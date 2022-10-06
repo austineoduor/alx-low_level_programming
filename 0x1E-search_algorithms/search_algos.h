6736a04daeb092a792860721cee4cbce1f0442da
austineoduor <austineoduor57@gmail.com>
2022-10-06 09:32:36 +0300

    file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,12 +14,12 @@
  *
  * Description: singly linked list node structure with an express lane
  */
- typedef struct skiplist_s
+typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 /**
@@ -31,11 +31,11 @@
  *
  * Description: singly linked list node structure
  */
- typedef struct listint_s
+typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 int linear_search(int *array, size_t size, int value);
